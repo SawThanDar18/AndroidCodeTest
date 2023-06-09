111342803b0437293eab5d98c24a9f3209a439c6
SawThanDar18 <sawthandar1998.pw@gmail.com>
2023-06-09 18:41:21 +0630

    added LogIn UI

--- app/src/main/java/com/sawthandar/androidcodetest/MainActivity.kt
+++ app/src/main/java/com/sawthandar/androidcodetest/MainActivity.kt
@@ -2,10 +2,33 @@ package com.sawthandar.androidcodetest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.core.view.isVisible
+import com.sawthandar.androidcodetest.databinding.ActivityLogInBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLogInBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityLogInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        uiListener()
+    }
+
+    private fun uiListener() {
+        binding.loginBtn.setOnClickListener {
+            //start teammate list activity
+            checkUserInputNull()
+        }
+    }
+
+    private fun checkUserInputNull() {
+        val fullInfo = (binding.companyIdEdt.text.toString().isNotBlank() && binding.usernameEdt.text.toString().isNotBlank() && binding.passwordEdt.text.toString().isNotBlank())
+        if (fullInfo) {
+        } else {
+        }
     }
 }
